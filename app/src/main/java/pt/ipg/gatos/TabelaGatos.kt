8599cf9081d9c09e0af20ed2ea74a7d8a0a74784
andregod <andre_godinho1@hotmail.com>
2023-05-05 10:17:19 +0100

    Acrescento do ON DELETE RESTRICT

--- app/src/main/java/pt/ipg/gatos/TabelaGatos.kt
+++ app/src/main/java/pt/ipg/gatos/TabelaGatos.kt
@@ -10,6 +10,6 @@ class TabelaGatos(db: SQLiteDatabase) : TabelaBD(db, NOME_TABELA) {
     }
 
     override fun criar() {
-        db.execSQL("CREATE TABLE $NOME_TABELA ($CHAVE_TABELA, titulo TEXT NOT NULL, isbn TEXTO, id_categoria INTEGER NOT NULL), FOREIGN KEY (id_categoria) REFERENCES ${TabelaCategorias.NOME_TABELA}(${BaseColumns._ID}) ")
+        db.execSQL("CREATE TABLE $NOME_TABELA ($CHAVE_TABELA, titulo TEXT NOT NULL, isbn TEXTO, id_categoria INTEGER NOT NULL), FOREIGN KEY (id_categoria) REFERENCES ${TabelaCategorias.NOME_TABELA}(${BaseColumns._ID}) ON DELETE RESTRICT ")
     }
 }
