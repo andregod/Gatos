e43c3611d4843ab02b95da056ce2d5d24c1b23c2
andregod <andre_godinho1@hotmail.com>
2023-05-09 10:13:55 +0100

    Adicionar teste para verificar funcionamento da BD

--- app/src/androidTest/java/pt/ipg/gatos/BDInstrumentedTest.kt
+++ app/src/androidTest/java/pt/ipg/gatos/BDInstrumentedTest.kt
@@ -1,5 +1,6 @@
 package pt.ipg.gatos
 
+import android.content.Context
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -14,11 +15,20 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class BDInstrumentedTest {
+    private fun getAppContext() : Context =
+        InstrumentationRegistry.getInstrumentation().targetContext
     @Test
-    fun useAppContext() {
+    fun consegueAbrirBaseDados() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+
+        val openHelper = BdGatosOpenHelper(getAppContext())
+        val bd= openHelper.readableDatabase
+        assert(bd.isOpen)
+
+        val appContext = getAppContext()
         assertEquals("pt.ipg.gatos", appContext.packageName)
     }
+
+
 }
