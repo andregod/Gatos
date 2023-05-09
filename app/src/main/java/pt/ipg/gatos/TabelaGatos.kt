d3422098b2484bf32fd4c5a79ff823fa032dc715
andregod <andre_godinho1@hotmail.com>
2023-05-09 09:57:14 +0100

    Fix da virgula e de strings

--- app/src/main/java/pt/ipg/gatos/TabelaGatos.kt
+++ app/src/main/java/pt/ipg/gatos/TabelaGatos.kt
@@ -6,10 +6,10 @@ import android.provider.BaseColumns
 
 class TabelaGatos(db: SQLiteDatabase) : TabelaBD(db, NOME_TABELA) {
     companion object {
-        const val NOME_TABELA = "gatos"
+        const val NOME_TABELA = "Gatos"
     }
 
     override fun criar() {
-        db.execSQL("CREATE TABLE $NOME_TABELA ($CHAVE_TABELA, titulo TEXT NOT NULL, isbn TEXTO, id_categoria INTEGER NOT NULL), FOREIGN KEY (id_categoria) REFERENCES ${TabelaCategorias.NOME_TABELA}(${BaseColumns._ID}) ON DELETE RESTRICT ")
+        db.execSQL("CREATE TABLE $NOME_TABELA ($CHAVE_TABELA, titulo TEXT NOT NULL, isbn TEXT, id_categoria INTEGER NOT NULL, FOREIGN KEY (id_categoria) REFERENCES ${TabelaCategorias.NOME_TABELA}(${BaseColumns._ID}) ON DELETE RESTRICT)")
     }
 }
