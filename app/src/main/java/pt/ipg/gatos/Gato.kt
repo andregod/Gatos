24adc9bbe85bff2fa4aab638a5c497a2e64da0b4
andregod <andre_godinho1@hotmail.com>
2023-05-12 09:16:37 +0100

    Criar a função que converte os valores de um livro (gato) em ContentValues

--- app/src/main/java/pt/ipg/gatos/Gato.kt
+++ app/src/main/java/pt/ipg/gatos/Gato.kt
@@ -1,5 +1,7 @@
 package pt.ipg.gatos
 
+import android.content.ContentValues
+
 data class Gato(
     var titulo: String,
     var idCategoria: Int,
@@ -7,5 +9,14 @@ data class Gato(
     var id: Long = -1
 ) {
 
+    fun toContentValues(): ContentValues {
+        val valores = ContentValues()
+
+        valores.put(TabelaGatos.CAMPO_TITULO, titulo)
+        valores.put(TabelaGatos.CAMPO_ISBN, isbn)
+        valores.put(TabelaGatos.CAMPO_FK_CATEGORIA, idCategoria)
+
+        return valores
+    }
 
 }
--- app/src/main/java/pt/ipg/gatos/TabelaGatos.kt
+++ app/src/main/java/pt/ipg/gatos/TabelaGatos.kt
@@ -5,11 +5,15 @@ import android.provider.BaseColumns
 
 
 class TabelaGatos(db: SQLiteDatabase) : TabelaBD(db, NOME_TABELA) {
-    companion object {
-        const val NOME_TABELA = "Gatos"
-    }
 
     override fun criar() {
-        db.execSQL("CREATE TABLE $NOME_TABELA ($CHAVE_TABELA, titulo TEXT NOT NULL, isbn TEXT, id_categoria INTEGER NOT NULL, FOREIGN KEY (id_categoria) REFERENCES ${TabelaCategorias.NOME_TABELA}(${BaseColumns._ID}) ON DELETE RESTRICT)")
+        db.execSQL("CREATE TABLE $NOME_TABELA ($CHAVE_TABELA, $CAMPO_TITULO TEXT NOT NULL, $CAMPO_ISBN TEXT, $CAMPO_FK_CATEGORIA INTEGER NOT NULL, FOREIGN KEY ($CAMPO_FK_CATEGORIA) REFERENCES ${TabelaCategorias.NOME_TABELA}(${BaseColumns._ID}) ON DELETE RESTRICT)")
+    }
+
+    companion object {
+        const val NOME_TABELA = "livros"
+        const val CAMPO_TITULO = "titulo"
+        const val CAMPO_ISBN = "isbn"
+        const val CAMPO_FK_CATEGORIA = "id_categoria"
     }
 }
