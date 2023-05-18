c1c296f94b0ea94960ec2fa69e5928724cf550b4
andregod <74107774+andregod@users.noreply.github.com>
2023-05-18 15:42:18 +0100

    Correção e adaptação da TabelaCategorias(Raça)

--- app/src/main/java/pt/ipg/gatos/TabelaCategorias.kt
+++ app/src/main/java/pt/ipg/gatos/TabelaCategorias.kt
@@ -7,14 +7,19 @@ import android.provider.BaseColumns
 
 class TabelaCategorias(db: SQLiteDatabase) : TabelaBD(db, "Categorias") {
     override fun criar() {
-        db.execSQL("CREATE TABLE $NOME_TABELA ($CHAVE_TABELA, descricao TEXT NOT NULL)")
+        db.execSQL("CREATE TABLE $NOME_TABELA ($CHAVE_TABELA, $CAMPO_NOMERACA TEXT NOT NULL, $CAMPO_CORPRINCIPALRACA TEXT NOT NULL, $CAMPO_PORTERACA TEXT NOT NULL)")
     }
 
     companion object {
         const val NOME_TABELA = "categorias"
-        const val CAMPO_DESCRICAO = "descricao"
 
-        val CAMPOS = arrayOf (BaseColumns._ID, CAMPO_DESCRICAO)
+        const val CAMPO_NOMERACA = "nome_raca"
+        const val CAMPO_CORPRINCIPALRACA = "cor_principal_raca"
+        const val CAMPO_PORTERACA = "porte_raca"
+        //const val CAMPO_DESCRICAO = "descricao"
+
+        //val CAMPOS = arrayOf (BaseColumns._ID, CAMPO_DESCRICAO)
+        val CAMPOS = arrayOf (BaseColumns._ID, CAMPO_NOMERACA, CAMPO_CORPRINCIPALRACA, CAMPO_PORTERACA)
 
     }
 }
