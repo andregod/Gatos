6663e1474404c6736e7bb3db7f556f132f3fada5
andregod <andre_godinho1@hotmail.com>
2023-05-26 09:18:59 +0100

    Criada a classe adaptador de gatos que gera a interface recyclerview

--- app/src/main/java/pt/ipg/gatos/MainActivity.kt
+++ app/src/main/java/pt/ipg/gatos/MainActivity.kt
@@ -30,7 +30,7 @@ class MainActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
-        
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
