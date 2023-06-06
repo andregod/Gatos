0095fc520ff0dca5b7fd80384e336389d5a947a2
andregod <74107774+andregod@users.noreply.github.com>
2023-06-07 00:23:51 +0100

    Adaptações classes

--- app/src/main/java/pt/ipg/gatos/NovoGatoFragment.kt
+++ app/src/main/java/pt/ipg/gatos/NovoGatoFragment.kt
@@ -8,9 +8,8 @@ import androidx.fragment.app.Fragment
 import pt.ipg.gatos.MainActivity
 import pt.ipg.gatos.R
 import pt.ipg.gatos.databinding.FragmentNovoGatoBinding
-import pt.ipg.livros.databinding.FragmentNovoLivroBinding
 
-class NovoLivroFragment : Fragment() {
+class NovoGatoFragment : Fragment() {
     private var _binding: FragmentNovoGatoBinding? = null
 
     // This property is only valid between onCreateView and
