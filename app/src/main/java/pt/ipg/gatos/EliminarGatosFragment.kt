5fc1088cf52a83881eecdd29b9d86cbc1fdac108
andregod <andre_godinho1@hotmail.com>
2023-06-13 09:26:42 +0100

    Interface do Fragmento EliminarGatosFragment

--- app/src/main/java/pt/ipg/gatos/EliminarGatosFragment.kt
+++ app/src/main/java/pt/ipg/gatos/EliminarGatosFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import pt.ipg.gatos.databinding.FragmentNovoGatoBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,24 +19,24 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class EliminarGatosFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: FragmentNovoGatoBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_eliminar_gatos, container, false)
+
+        _binding = FragmentNovoGatoBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     companion object {
@@ -56,4 +58,10 @@ class EliminarGatosFragment : Fragment() {
                 }
             }
     }
+
+    private fun voltaListaGatos() {
+        findNavController().navigate(R.id.action_eliminarLivroFragment_to_ListaGatosFragment)
+    }
+
+    private fun eliminar() {}
 }
